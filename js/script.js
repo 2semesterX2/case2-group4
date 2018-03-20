bff6b38001995282ef7de89e777113acc7642f8e
Elis <elis.laasik@gmail.com>
2018-03-20 11:54:18 +0100

    javascript storyline
    well acutally we are going to be using jquery

---
+++ js/script.js
@@ -0,0 +1,31 @@
+
+// if page is loaded then only show the frontpage and id=headline
+// if 'start' is pressed show only section id=audio and id=headline
+// when continue is pressed show only section id=story1 and id=headline
+// start video automatically after 1 second
+// when video ends show only section id=game1 and id=headline 
+//when a correct picture is pressed show only id=correct1 else when the wrong image is pressed show id=incorrect1 and always show section id=headline
+
+
+// when continue is pressed show only section id=story2 and id=healdline 
+//start video automatically after 1 second
+// when video ends show only section id=game2 and id=healine
+// when when a correct picture is pressed show only id=correct2 else when the wrong image is pressed show id=incorrect2 and always show section id=headline
+//when 'continue' is pressed show only section id=fork and id=headline
+//when 'sell it' pressed show only section id=auction else when 'report it to the authorities is' pressed show only section id=arttheftsquad and id=headline
+
+//make a header  then it is always 
+//$ section.hide
+//$ id secton .show if onclick
+//.on click name of the function
+/*
+$
+hide all sections
+faq example
+
+.delay. hide
+controling video from jquery
+*/
+// ADD ID'S TO CONTINUE BUTTONS!!!!
+// Should the show always section id=headline be defiened before or said on every section of code separaly?
+// Should the correct incorrect be two sections of code or one done with if else?
